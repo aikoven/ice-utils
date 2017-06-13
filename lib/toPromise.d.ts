2eead9098214bdd2190e68c4953203b8c57790ce
Daniel Lytkin <gaelco.salmon@gmail.com>
2017-06-13 12:14:06 +0700

    Cleanup

--- lib/toPromise.d.ts
+++
@@ -1,8 +0,0 @@
-import { Ice } from "ice";
-/**
- * Turn Ice promise to normal one.
- *
- * @param promise Ice promise.
- * @returns Native promise.
- */
-export default function toPromise<T>(promise: Ice.Promise<T>): Promise<T>;
